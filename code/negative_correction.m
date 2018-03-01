240cc14c1d7cd8fc7215b86d6daacf28c79a8ef3
jakebeal <jakebeal@alum.mit.edu>
2018-03-01 05:33:29 -0600

    clean up negative_correction function

--- code/negative_correction.m
+++ code/negative_correction.m
@@ -1,10 +1,7 @@
+function corrected = negative_correction(raw,sigma)
+
 % cumulative normal from 0 to infinity of measurement -x, with std. dev. sigma
 % expectation of one-sided truncation:
-
-function corrected = negative_correction(raw,sigma)
-%sigma = 100;
-%raw = -300:1:1000;
-
 phi = @(x)(1/(sqrt(2*pi))*exp(-0.5*(x.^2)));
 Phi = @(x)(0.5*(1+erf(x/sqrt(2))));
 
@@ -12,7 +9,3 @@ a = 0;
 alpha = (a-raw)/sigma;
 Z = 1-Phi(alpha);
 corrected = raw + sigma*phi(alpha)./Z;
-
-% figure;
-% plot(raw,E_x);
-% xlabel('raw'); ylabel('estimated');
