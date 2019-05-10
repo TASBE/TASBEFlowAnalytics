46966d2a61f45cd8c05f0419ab95df4cc87e9357
jakebeal <jakebeal@alum.mit.edu>
2019-05-10 15:41:58 -0500

    First functional version of negative correction; still needs refactoring and integration into pipeline

--- code/negative_correction.m
+++ code/negative_correction.m
@@ -1,42 +1,48 @@
-function corrected = negative_correction(raw,sigma)
+function [corrected,cum_prob] = negative_correction(raw,sigma)
 
-% cumulative normal from 0 to infinity of measurement -x, with std. dev. sigma
-% expectation of one-sided truncation:
-phi = @(x)(1/(sqrt(2*pi))*exp(-0.5*(x.^2)));
-Phi = @(x)(0.5*(1+erf(x/sqrt(2))));
-
-a = 0;
-alpha = (a-raw)/sigma;
-Z = 1-Phi(alpha);
-corrected = raw + sigma*phi(alpha)./Z;
-
+% Building the model is expensive, takes time proportional to sigma, and can be done with an autofluorescence model
+tic
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Let's try to compute this another way:
 % Go up to k sigmas, see what the probability of it coming from this value is, assuming an even log distribution from 1-4sigma
 bound = 4*sigma;
-range = -bound:2*bound;
-expectation = zeros(size(range));
+range = -bound:8*bound;
+inverse_grain = 0.01;
+inverse_range = 0:inverse_grain:log10(bound*8);
+cum_prob = zeros(numel(range),numel(inverse_range));
 for i=1:numel(range);
-    expectation(i) = 0;
-    cum_prob = 0;
-    for j=0:0.01:log10(bound*10)
-        prob = p_gaussian(range(i)-0.5,range(i)+0.5,10^(j+0.005),sigma);
-        cum_prob = cum_prob + prob;
-        expectation(i) = expectation(i) + (10.^(j+0.005))*prob;
+    range_prob = zeros(size(inverse_range));
+    for j=1:numel(inverse_range)
+        range_prob(j) = p_gaussian(range(i)-0.5,range(i)+0.5,10^(inverse_range(j)-inverse_grain/2),sigma);
     end
-    expectation(i) = expectation(i)/cum_prob;
+    cum_prob(i,:) = cumsum(range_prob)/sum(range_prob); % normalized distribution
 end
+toc
 
-figure;
-plot(range,expectation);
-
-figure;
-plot(range,log10(range)-log10(expectation))
+% Applying is proportional to both sigma and number of events, and is fairly fast
+tic
 
 rescales = (raw<max(range) & raw>min(range));
 underflow = raw<=min(range);
 corrected = raw;
-corrected(underflow) = expectation(1);
-corrected(rescales) = expectation(bound+ceil(0.5+raw(rescales)));
+corrected(underflow) = 1; % 10^0
 
+n_rescales = sum(rescales);
+rescale_projection = rand(n_rescales,1);
+old_rescale_values = raw(rescales);
+rescale_probability = 1-((old_rescale_values/(max(range)/2))-1);
+rescale_feathering = rand(n_rescales,1);
+rescale_indices = bound+ceil(0.5+old_rescale_values);
+rescale_values = nan(n_rescales,1);
+rescale_dequantize = (rand(n_rescales,1)-1)*inverse_grain; % randomly select within the range to the mark
+for i=1:n_rescales
+    if rescale_feathering(i) < rescale_probability(i)
+        rescale_values(i) = 10.^(rescale_dequantize(i) + inverse_range(find(rescale_projection(i)<cum_prob(rescale_indices(i),:),1)));
+    else
+        rescale_values(i) = old_rescale_values(i);
+    end
+end
+
+corrected(rescales) = rescale_values;
+toc
 
--- tests/test_negative_correction.m
+++ tests/test_negative_correction.m
@@ -1,6 +1,6 @@
 % make a synthetic data set:
 sigma = 100;
-n_events = 1e5;
+n_events = 1e6;
 noise = randn(n_events,1)*sigma;
 log_range = [0 5];
 baseline = 10.^(log_range(1) + (rand(n_events,1)*(log_range(2)-log_range(1))));
@@ -20,9 +20,16 @@ figure; hist(log10(cumulative(cumulative>0)),1e2);
 figure; hist(cumulative(cumulative<800),1e2);
 
 
-corrected = negative_correction(cumulative,sigma);
-figure;
-hist(log10(corrected),1e2);
+[corrected,cum_prob] = negative_correction(cumulative,sigma);
+figure; hist(log10(corrected),1e2);
+
+% make sure the distribution is nicely flat
+n = hist(log10(corrected),1e2);
+assertElementsAlmostEqual(n,10000*ones(1,100),0.05,'relative');
+
+figure; hist(log10(corrected(corrected<1000)),1e2);
+
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Can we decide what an ideal corrective plot would be?
